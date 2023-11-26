4656f45b413f91658f65bb9a336938eacfb2f7ad
Mohammad <mohammadrasel201@gmail.com>
2023-11-26 06:10:54 +0600

    Added See more and Total Price Section

--- src/Pages/Favourites/Favourites.jsx
+++ src/Pages/Favourites/Favourites.jsx
@@ -3,21 +3,32 @@ import Phone from "../Phone/Phone";
 import PhoneCard from "../Phone/PhoneCard";
 
 const Favourites = () => {
-    const [favourites,setFavourites] = useState([])
+    const [favourites,setFavourites] = useState([]);
 
-    const [noFound,setNoFound] = useState("")
+    const [noFound,setNoFound] = useState("");
+
+    const [isShow,setIsShow] = useState(false);
+
+    const [totalPrice,setTotalPrice] = useState(0)
 
     useEffect(()=>{
         const favouriteItems = JSON.parse(localStorage.getItem("favourites"));
 
         if(favouriteItems){
-            setFavourites(favouriteItems)
+            setFavourites(favouriteItems)        
+
+        const total = favouriteItems.reduce((previousValue,currentValue)=> previousValue + currentValue.price,0)
+        
+        console.log(total);
+
+        setTotalPrice(total)
+
         }
 
         else {
             setNoFound('No Data Found')
         }
-    },[])
+    },[]);
 
 
     console.log(favourites);
@@ -28,6 +39,8 @@ const Favourites = () => {
         setNoFound("No Data Found");
     }
 
+    console.log(isShow)
+
     return (
         <div>
             {noFound ? <p className=" h-[80vh] flex justify-center items-center">{noFound}</p>
@@ -36,21 +49,34 @@ const Favourites = () => {
 
                 {favourites.length > 0 && (
                 
-                <button
-                 onClick={handleRemove}
-                 className="px-5 bg-green-200 block mx-auto p-2 rounded"
-                 >
-                    Delete All Favourite
-                </button>
+                <div>
+                    <button
+                        onClick={handleRemove}
+                        className="px-5 bg-green-200 block mx-auto p-2 rounded"
+                        >
+                         Delete All Favourite
+                    </button>
+
+                    <h2>Total Price : {totalPrice}</h2>
+                </div>
+
                 )}
 
                 <div className="grid grid-cols-2 gap-5">
-                    {favourites.map((phone) => (
-                    <PhoneCard key={phone.id} phone={phone}></PhoneCard>))}
+                   { 
+                        isShow ? favourites.map((phone) => (
+                        <PhoneCard key={phone.id} phone={phone}></PhoneCard>))
+                        :
+                        favourites.slice(0,4).map((phone) => (
+                        <PhoneCard key={phone.id} phone={phone}></PhoneCard>))
+                    }
                 </div>
 
-                <button className="px-5 bg-green-200 block mx-auto p-2 rounded">See More</button>
-
+                    {favourites.length > 4 && 
+                        <button onClick={()=>setIsShow(!isShow)} className="px-5 bg-green-200 block mx-auto p-2 rounded">
+                            {isShow ? "See Less" : "See More"}
+                        </button>
+}
 
             </div>
             )}
